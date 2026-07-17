3e34b7f4b47a92c9a21271a3c44a5b838faaa474
margalhao <margalhao21@gmail.com>
2026-07-17 01:29:04 -0300

    v2.0

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
-const buttons = document.querySelectorAll('.choice')
-const playerScore = document.querySelector('.your-score span')
-const machineScore = document.querySelector('.machine-score span')
+const buttons = document.querySelectorAll('.choice');
+const playerScore = document.querySelector('.your-score span');
+const machineScore = document.querySelector('.machine-score span');
 const result = document.querySelector('.result');
 const reset = document.querySelector('.reset');
+const translate = {
+    rock: 'pedra',
+    paper: 'papel',
+    scissor: 'tesoura'
+}
 const choices = ['rock', 'paper', 'scissor'];
 const getMachineChoice = () => choices[Math.floor(Math.random() * choices.length)];
 
@@ -10,28 +15,27 @@ let playerPoints = 0
 let machinePoints = 0
 let message = 'Resultado'
 
-
 const render = () => {
     playerScore.textContent = playerPoints
     machineScore.textContent = machinePoints
-    result.textContent = message
+    result.innerHTML = message
 }
 render()
 
 const play = (playerChoice) => {
     const machineChoice = getMachineChoice()
     if (playerChoice === machineChoice) {
-        message = 'Empate'
+        message = `Empate a máquina escolheu ${translate[machineChoice]}!`
         render()
     } else if ((playerChoice === 'rock' && machineChoice === 'scissor') ||
               (playerChoice === 'paper' && machineChoice === 'rock') || 
               (playerChoice === 'scissor' && machineChoice === 'paper')) {
         playerPoints++
-        message = 'Vitória'
+        message = `Vitória <br> A máquina escolheu ${translate[machineChoice]}!`
         render()
     } else {
         machinePoints++
-        message = 'Derrota'
+        message = `Derrota <br> A máquina escolheu ${translate[machineChoice]}!`
         render()
     }
 }
@@ -43,6 +47,6 @@ buttons.forEach((button) => {
 reset.addEventListener('click', () => {
     playerPoints = 0
     machinePoints = 0
-    message = 'Resultado'
+    message = Resultado
     render()
 })
